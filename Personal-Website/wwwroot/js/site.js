7b938a7e5f8c5a7dca6c031220988a90e4896051
James <jamessteven301@gmail.com>
2018-09-06 16:57:02 -0400

    Added hover js function

--- Personal-Website/wwwroot/js/site.js
+++ Personal-Website/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $("p").hover(function () {
+        $(this).css("background-color", "yellow");
+    }, function () {
+        $(this).css("background-color", "pink");
+    });
+});
